cfd9ba53422a88c6a16aed886a88f6a7de5a0f1b
pavyakrish <pavyakrishnamoorthy@gmail.com>
2016-09-16 19:54:35 +0530

    reverse

---
+++ Reverse.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Reverse
+{
+public static void main(String args[])]
+
+{
+Scanner in = new Scanner(System.in);
+System.out.println("Enter the number of rows which you want in your Floyd Triangle: ");
+String r = in.nextLine();
+String s="";
+strrev(r,s);
+if(r==s)
+{
+System.out.println("s");
+}
+else
+{
+System.out.println("n");
+}
+}
+}
